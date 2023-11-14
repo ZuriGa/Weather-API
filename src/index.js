9f26900285d89e62ca6662dfbbe30d8bce95d7c1
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-14 11:19:44 -0800

    final commit
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,2 +1,72 @@
 import './css/styles.css';
-jaklsjflsa
+
+// Business Logic
+function getWeather(city, zip) {
+  let request = new XMLHttpRequest();
+  let url;
+
+  if (city && zip) {
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  } else if (city) {
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  } else if (zip) {
+    url =`http://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=${process.env.API_KEY}`;
+  }
+  else {
+    printError(null, { error: "invalid text"}, city || zip);
+    return;
+  }
+
+
+  request.addEventListener("loadend", function () {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, city, zip);
+    } else {
+      printError(this, response, city, zip);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+function convertKelvinToFahrenheit(kelvin) {
+  return ((kelvin - 273.15) * 9 / 5 + 32).toFixed(2);
+}
+
+function convertEpoch(epoch) {
+  const epochMilliseconds = epoch * 1000;
+  const date = new Date(epochMilliseconds)
+  return date
+}
+
+// UI Logic
+
+function printElements(apiResponse, city) {
+  const kelvinTemp = apiResponse.main.temp
+  const fahrenheitTemp = convertKelvinToFahrenheit(kelvinTemp)
+  const epochTime = apiResponse.sys.sunset
+  const standardTime = convertEpoch(epochTime)
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
+    The temperature in Kelvins is ${apiResponse.main.temp} degrees.
+    The temperature in Fahrenheit is ${fahrenheitTemp} degrees. 
+    The wind speed will be ${apiResponse.wind.speed}.
+    The sunset will be ${standardTime}`;
+}
+
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
+// const ESLintPlugin = require('eslint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 
@@ -18,14 +18,14 @@ module.exports = {
     },
     devtool: 'eval-source-map',
     plugins: [
-        new Dotenv(),
-        new ESLintPlugin(),
+        // new ESLintPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Project', // this line will be the title that shows up in your browser tab
             template: './src/index.html', // everything will get injected into this file
             inject: 'body',
         }),
+        new Dotenv()
     ],
 
     module: {
